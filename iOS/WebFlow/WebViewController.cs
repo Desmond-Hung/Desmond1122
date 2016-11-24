c004a49bb70225f4ff6a883ed131139491e4a096
DesmondHung <desmond.hung@jetfusion.com.tw>
2016-11-24 16:37:41 +0800

    修正縮排與註解

--- iOS/WebFlow/WebViewController.cs
+++ iOS/WebFlow/WebViewController.cs
@@ -16,6 +16,7 @@ namespace Desmond.iOS
 		{
 			base.ViewDidLoad();
 
+			// 利用WebView連網頁後，利用網址的變化來分析字串，啟動Local端Function
 			viewWeb.LoadHtmlString(@"
 			<html>
 				<head>
@@ -33,37 +34,35 @@ namespace Desmond.iOS
 				</body>
 			</html>", null);
 
-			viewWeb.ShouldStartLoad =
-				delegate (UIWebView webView, NSUrlRequest request, UIWebViewNavigationType navigationType)
+			viewWeb.ShouldStartLoad = delegate (UIWebView webView, NSUrlRequest request, UIWebViewNavigationType navigationType)
+			{
+				Console.WriteLine("ShouldStartLoad");
+				var requestString = request.Url.AbsoluteString;
+				var components = requestString.Split(new[] { @"://" }, StringSplitOptions.None);
+				if (components.Length > 1 && components[0].ToLower() == @"shirly".ToLower())
 				{
-					var requestString = request.Url.AbsoluteString;
-					var components = requestString.Split(new[] { @"://" }, StringSplitOptions.None);
-					if (components.Length > 1 && components[0].ToLower() == @"shirly".ToLower())
+					if (components[1] == @"Hi")
 					{
-
-						if (components[1] == @"Hi")
+						UIAlertController alert = UIAlertController.Create(@"Hi Title", @"當然是世界好", UIAlertControllerStyle.Alert);
+						UIAlertAction okAction = UIAlertAction.Create(@"OK", UIAlertActionStyle.Default, (action) =>
 						{
+							Console.WriteLine(@"OK");
+						});
+						alert.AddAction(okAction);
 
-							UIAlertController alert = UIAlertController.Create(@"Hi Title", @"當然是世界好", UIAlertControllerStyle.Alert);
-							UIAlertAction okAction = UIAlertAction.Create(@"OK", UIAlertActionStyle.Default, (action) =>
-							{
-								Console.WriteLine(@"OK");
-							});
-							alert.AddAction(okAction);
+						UIAlertAction cancelAction = UIAlertAction.Create(@"Cancel", UIAlertActionStyle.Default, (action) =>
+						{
+							Console.WriteLine(@"Cancel");
+						});
+						alert.AddAction(cancelAction);
 
-							UIAlertAction cancelAction = UIAlertAction.Create(@"Cancel", UIAlertActionStyle.Default, (action) =>
-							{
-								Console.WriteLine(@"Cancel");
-							});
-							alert.AddAction(cancelAction);
+						PresentViewController(alert, true, null);
 
-							PresentViewController(alert, true, null);
-
-							return false;
-						}
+						return false;
 					}
-					return true;
-				};
+				}
+				return true;
+			};
 		}
 
 		public override void DidReceiveMemoryWarning()
